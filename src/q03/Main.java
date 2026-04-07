e6e86d48909724e5af6e5d5226230a89576943e9
Prasoon Gupta <129424171+Gupta-Prasoon@users.noreply.github.com>
2026-04-07 14:44:28 +0530

    Refactor spiral matrix generation and diagonal sum

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,41 +4,61 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+
         int[][] mat = new int[n][n];
 
-        int num = 1;
         int top = 0, bottom = n - 1;
         int left = 0, right = n - 1;
+        int num = 1;
 
-        while (num <= n * n) {
-            
-            for (int i = left; i <= right && num <= n * n; i++) mat[top][i] = num++;
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++) {
+                mat[top][i] = num++;
+            }
             top++;
-        
-            for (int i = top; i <= bottom && num <= n * n; i++) mat[i][right] = num++;
+
+
+            for (int i = top; i <= bottom; i++) {
+                mat[i][right] = num++;
+            }
             right--;
-           
-            for (int i = right; i >= left && num <= n * n; i--) mat[bottom][i] = num++;
-            bottom--;
-           
-            for (int i = bottom; i >= top && num <= n * n; i--) mat[i][left] = num++;
-            left++;
+
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    mat[bottom][i] = num++;
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    mat[i][left] = num++;
+                }
+                left++;
+            }
         }
 
-      
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(mat[i][j]);
-                if (j != n - 1) System.out.print(" ");
+                if (j < n - 1) System.out.print(" ");
             }
             System.out.println();
         }
 
-        
-        int diagSum = 0;
-        for (int i = 0; i < n; i++) diagSum += mat[i][i];
-        System.out.println("Diagonal: " + diagSum);
+        int diag = 0;
 
-        sc.close();
+        for (int i = 0; i < n; i++) {
+            diag += mat[i][i];           
+            diag += mat[i][n - i - 1];   
+        }
+
+        if (n % 2 == 1) {
+            diag -= mat[n / 2][n / 2];
+        }
+
+        System.out.println("Diagonal: " + diag);
     }
 }
