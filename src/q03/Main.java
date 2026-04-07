a180227cb7ef75d02ac754714c87f31c868015f3
Prasoon Gupta <129424171+Gupta-Prasoon@users.noreply.github.com>
2026-04-07 14:34:39 +0530

    Fill matrix in spiral order and calculate diagonal sum
    Implemented a method to fill an N×N matrix in clockwise spiral order and calculate the sum of the primary diagonal.

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -4,17 +4,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int[][] mat = new int[n][n];
 
-        // TODO: Fill an N×N matrix in clockwise spiral order starting from 1
-        //       Print each row with values separated by single space
-        //       Then print: "Diagonal: X" where X = sum of primary diagonal (top-left to bottom-right)
-        //
-        // Input: 3
-        // Output:
-        // 1 2 3
-        // 8 9 4
-        // 7 6 5
-        // Diagonal: 15
+        int num = 1;
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
 
+        while (num <= n * n) {
+            
+            for (int i = left; i <= right && num <= n * n; i++) mat[top][i] = num++;
+            top++;
+        
+            for (int i = top; i <= bottom && num <= n * n; i++) mat[i][right] = num++;
+            right--;
+           
+            for (int i = right; i >= left && num <= n * n; i--) mat[bottom][i] = num++;
+            bottom--;
+           
+            for (int i = bottom; i >= top && num <= n * n; i--) mat[i][left] = num++;
+            left++;
+        }
+
+      
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(mat[i][j]);
+                if (j != n - 1) System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        
+        int diagSum = 0;
+        for (int i = 0; i < n; i++) diagSum += mat[i][i];
+        System.out.println("Diagonal: " + diagSum);
+
+        sc.close();
     }
 }
