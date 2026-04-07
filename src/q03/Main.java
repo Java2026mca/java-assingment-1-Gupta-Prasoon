ab806f6c87560f1781aefe8db10df0ab30e48aca
Prasoon Gupta <129424171+Gupta-Prasoon@users.noreply.github.com>
2026-04-07 14:49:30 +0530

    Refactor spiral matrix population and diagonal sum

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -10,26 +10,33 @@ public class Main {
         int top = 0, bottom = n - 1;
         int left = 0, right = n - 1;
         int num = 1;
-        
+
+
         while (top <= bottom && left <= right) {
 
-            for (int i = left; i <= right; i++)
+
+            for (int i = left; i <= right; i++) {
                 mat[top][i] = num++;
+            }
             top++;
 
-            for (int i = top; i <= bottom; i++)
+
+            for (int i = top; i <= bottom; i++) {
                 mat[i][right] = num++;
+            }
             right--;
 
             if (top <= bottom) {
-                for (int i = right; i >= left; i--)
+                for (int i = right; i >= left; i--) {
                     mat[bottom][i] = num++;
+                }
                 bottom--;
             }
 
             if (left <= right) {
-                for (int i = bottom; i >= top; i--)
+                for (int i = bottom; i >= top; i--) {
                     mat[i][left] = num++;
+                }
                 left++;
             }
         }
@@ -45,10 +52,6 @@ public class Main {
         int diag = 0;
         for (int i = 0; i < n; i++) {
             diag += mat[i][i];
-        }
-
-        if (n > 3) {
-            diag += mat[n-1][0];
         }
 
         System.out.println("Diagonal: " + diag);
